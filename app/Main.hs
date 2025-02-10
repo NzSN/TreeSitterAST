42c2d6afea51359bb53e9604200cd29033d634e6
NzSN <nzsn0101@gmail.com>
2025-02-11 07:59:42 +0800

    fixup

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,6 @@ module Main where
 import System.Environment (getArgs)
 import Control.Monad.Trans.Maybe
 
-import qualified MyLib (decode_node_types)
 import qualified TreeSitterNodes as TSN (parse_node_types)
 
 main :: IO ()
