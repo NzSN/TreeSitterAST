861119cc434f928985a6c576bf4bf8e246613af9
NzSN <nzsn0101@gmail.com>
2025-04-08 12:08:41 +0800

    Remove Backend and Add Tests for node-types.json parser

--- src/Backends.hs
+++
@@ -1,8 +0,0 @@
-module Backends (Backend(..)) where
-
-import qualified Data.Aeson as Aeson
-import qualified Data.ByteString.Lazy as BSL
-
-class Backend a where
-  name :: a -> String
-  gen_ast :: a -> [Aeson.Object] -> BSL.ByteString
