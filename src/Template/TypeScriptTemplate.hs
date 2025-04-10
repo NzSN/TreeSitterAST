bd4b028dcbedfea5a3619bac0830c0f4187a62a0
NzSN <nzsn0101@gmail.com>
2025-04-10 18:00:28 +0800

    Implement templates

--- src/Template/TypeScriptTemplate.hs
+++ src/Template/TypeScriptTemplate.hs
@@ -9,3 +9,38 @@ import Data.Text.Lazy (Text)
 
 import_statement :: Template (TArray Text -> Text -> String)
 import_statement = T $ "import { " % (commaSep build) % " } from '" % text % "';"
+
+type IsExport = Bool
+type IsConst  = Bool
+variable_decl :: IsExport -> IsConst -> Template (Text -> Text -> String)
+variable_decl True True   = T $ "export const " % text % " = \"" % text % "\";"
+variable_decl False True  = T $ "const "        % text % " = \"" % text % "\";"
+variable_decl True False  = T $ "export"        % text % " = \"" % text % "\";"
+variable_decl False False = T $ "let "          % text % " = \"" % text % "\";"
+
+type ParameterDeclareT = Text
+parameter_declare :: Template (Text -> Text -> String)
+parameter_declare = T $ text % " : " % text
+
+
+type TypeIdent = Text
+type FunctionIdent = Text
+type FunctionDeclare = Template (FunctionIdent ->
+                                 -- Parameter of functions
+                                 TArray ParameterDeclareT ->
+                                 -- Tail Return type of function
+                                 TypeIdent ->
+                                 -- Statements inside the body of Function
+                                 TArray (Template a)
+                                  )
+function_declare :: Template (FunctionIdent ->
+                               -- Parameter of functions
+                               TArray ParameterDeclareT ->
+                               -- Tail Return type of function
+                               TypeIdent ->
+                               -- Statements inside the body of Function
+                               TArray Text
+                               -- Output as string
+                               -> String)
+function_declare = T $
+  "function " % text %
