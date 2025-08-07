56e29fad7b588241f9ce301f352b8d4175554023
NzSN <nzsn0101@gmail.com>
2025-08-07 17:51:21 +0800

    Add TemplateFunction

--- src/Fundamentals/File.hs
+++ src/Fundamentals/File.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, BangPatterns #-}
 
 module Fundamentals.File (
   File(..),
@@ -12,8 +12,8 @@ import System.IO (hGetContents, hPutStr, withFile, IOMode(..))
 
 
 data IO_Method a = IO_M {
-  io_read  :: String -> IO String,
-  io_write :: String -> String -> IO a
+  io_read  :: !(String -> IO String),
+  io_write :: !(String -> String -> IO a)
 }
 disk_io_method :: IO_Method ()
 disk_io_method = IO_M
@@ -21,18 +21,21 @@ disk_io_method = IO_M
   (\path content -> withFile path WriteMode $ \h -> hPutStr h content)
 
 data File a = File {
-  path :: String,
-  io_method :: IO_Method a,
+  path :: !String,
+  io_method :: !(IO_Method a),
   -- Testing purposes
-  buffer :: a}
+  buffer :: !a }
 open_file :: String -> File ()
 open_file path = File path disk_io_method ()
 
+
+infixl <<-
 (<<-) :: IO (File a) -> String -> IO (File a)
 (<<-) f s =
   f >>= \f' -> io_write (io_method f') (path f') s
     >>= \content -> return $ File (path f') (io_method f') content
 
+infix ->>
 (->>) :: IO (File a) -> (String -> b) -> IO b
 (->>) file f = file
   >>= \file' -> io_read (io_method file') (path file')
---
+++ src/TemplateFunction/TemplateF.hs
@@ -0,0 +1,3 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TemplateFunction.TemplateF where
