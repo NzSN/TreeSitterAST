5910cc1fc38677799f7b0399d86b2679df264f21
NzSN <nzsn0101@gmail.com>
2026-01-29 12:03:35 +0800

    Support generate 'Code Generator' from grammar.json.

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,42 @@
 module Main where
 
 import Data.Maybe
-import System.Environment (getArgs)
 import Control.Monad.Trans.Maybe
-
-import System.IO
+import Options.Applicative
 
 import qualified TreeSitterNodes as TS
 import qualified BackendDescription.NodeDescription as BN
 import qualified BackendDescription.NodeProcessorDescription as BP
 
+data Mode = AstProc | CodeGen deriving (Show)
+
+modeParser :: Parser Mode
+modeParser = astProcFlag <|> codeGenFlag
+  where
+    astProcFlag = flag' AstProc (long "ast-proc" <> help "Generate both node_processor.ts and node_declare.ts")
+    codeGenFlag = flag' CodeGen (long "code-gen" <> help "Placeholder for future code generation")
+
+argsParser :: Parser (Mode, FilePath)
+argsParser = (,) <$> modeParser <*> strArgument (metavar "FILE" <> help "Path to node-types.json file")
+
+parserInfo :: ParserInfo (Mode, FilePath)
+parserInfo = info (argsParser <**> helper)
+  ( fullDesc
+  <> progDesc "Generate TypeScript files from Tree-sitter node-types.json"
+  <> header "TreeSitterAST - Tree-sitter AST code generator" )
+
+parseArgs :: IO (Mode, FilePath)
+parseArgs = execParser parserInfo
+
 main :: IO ()
 main = do
-  (path:_) <- getArgs
+  (mode, path) <- parseArgs
   content <- runMaybeT $ TS.parse_node_types path
 
   if isNothing content
-    then putStrLn "Fail to parse node-types.json"
-    else do
-      withFile "node_processor.ts" WriteMode $
-        \h -> hPutStr h $ BP.descript $ fromJust content
-      withFile "node_declare.ts" WriteMode $
-        \h -> hPutStr h $ BN.descript $ fromJust content
+    then error "Fail to parse node-types.json"
+    else case mode of
+      AstProc -> do
+        writeFile "node_processor.ts" (BP.descript $ fromJust content)
+        writeFile "node_declare.ts" (BN.descript $ fromJust content)
+      CodeGen -> putStrLn "Code generation not yet implemented"
